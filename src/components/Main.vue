5e12343e410043c05e2d37fbecfb3de5b6f5eae5
mukiwu <mukispace@gmail.com>
2021-08-03 09:44:34 +0800

    card 放入 item

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="d-flex justify-content-end">
+    <div class="d-flex justify-content-end my-5">
       <div class="nav btn-group" role="tablist">
         <button class="btn btn-outline-danger active" data-bs-toggle="tab" data-bs-target="#tabGrid" type="button" role="tab"><font-awesome-icon icon="th-large" /></button>
         <button class="btn btn-outline-danger" data-bs-toggle="tab" data-bs-target="#tabList" type="button" role="tab"><font-awesome-icon icon="list" /></button>
@@ -8,7 +8,23 @@
       
     </div>
     <div class="tab-content">
-      <div class="tab-pane fade show active" id="tabGrid" role="tabpanel">123</div>
+      <div class="tab-pane fade show active" id="tabGrid" role="tabpanel">
+        <div class="d-flex justify-content-between">
+          <div class="card" v-for="(item, key) in exercise" :key="key">
+            <div class="card-header">
+              {{ item.name }}
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <div class="card-footer text-muted">
+              2 days ago
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="tab-pane fade" id="tabList" role="tabpanel">456</div>
     </div>
   </div>
@@ -16,7 +32,19 @@
 
 <script>
 export default {
-  name: 'Main'
+  name: 'Main',
+  data() {
+    return {
+      exercise: [
+        { name: '課程 1',
+          tags: ["vue", "css", "html"]      
+        },{
+          name: '課程 2',
+          tags: ["vue", "JS"]
+        }
+      ]
+    }
+  }
 }
 </script>
 
