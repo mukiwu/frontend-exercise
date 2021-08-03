79052e3ed73d279ef91f42b6fe8f37cbda9cf291
mukiwu <mukispace@gmail.com>
2021-08-03 10:20:12 +0800

    顯示標籤

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -17,10 +17,11 @@
             <div class="card-body">
               <h5 class="card-title">Special title treatment</h5>
               <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-            <div class="card-footer text-muted">
-              2 days ago
+            <div class="card-footer">
+              <span class="badge bg-danger me-1" v-for="(tag, key) in item.tags" :key="key">
+                {{ tag }}
+              </span>
             </div>
           </div>
         </div>
